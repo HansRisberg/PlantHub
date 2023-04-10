4bfdedaabd5f8f61c628603e19602b2a02819edf
HaaVad <123933986+HaaVad@users.noreply.github.com>
2023-04-10 15:12:14 +0200

    minor class update, and API-controller added

--- PlantHub01/Models/Conversation.cs
+++ PlantHub01/Models/Conversation.cs
@@ -1,12 +1,18 @@
-﻿namespace PlantHub01.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlantHub01.Models
 {
     public class Conversation
     {
         public int Id { get; set; }
         //PlantId includes its owner into the conversation
+        [ForeignKey("Plant")]
         public int PlantId { get; set; }
+        public virtual Plant Plant { get; set; }
         //UserId of the person sending the request
+        [ForeignKey("User")]
         public int UserId { get; set; }
+        public virtual User User { get; set; }
         public string Title { get; set; } = string.Empty;
         public ICollection<Message>? Messages { get; set; }
     }
