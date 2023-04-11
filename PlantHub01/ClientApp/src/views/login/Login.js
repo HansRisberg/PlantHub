cdb632702bea8f7b733c9d17971398ec044c390d
nathalieschro <Nathalie.schro@gmail.com>
2023-04-11 16:10:00 +0200

    Started on setting up login

--- PlantHub01/ClientApp/src/views/login/Login.js
+++ PlantHub01/ClientApp/src/views/login/Login.js
@@ -1,15 +1,42 @@
 ﻿import * as React from 'react';
+import { useState } from 'react'; 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
 export const Login = () => {
+    const [user, setUser] = useState("");
+
+    async function handleLogin(event) {
+        event.preventDefault();
+        const requestBody = { "Username": user };
+        const test = JSON.stringify(requestBody);
+        console.log(test);
+        let jsonResponse = await fetch("https://localhost:7062/login", {
+            method: "POST",
+            //headers: {
+            //    "Content-Type": "application/json",
+            //},
+            body: JSON.stringify(requestBody),
+        })
+            //.then((response) => response.json())
+            //.then((result) => {
+            //    if (result.message === "OK") {
+            //        alert("you are logged in")
+            //    } else {
+            //        alert("Wrong username")
+            //    }
+            //})
+        let response = jsonResponse.json();
+    }
+
     return (
         <div>
             <h1>Login</h1>
             <Box
                 component="form"
+                onSubmit={handleLogin}
                 sx={{
                     '& > :not(style)': { m: 1, width: '25ch' },
                 }}
@@ -17,12 +44,11 @@ export const Login = () => {
                 autoComplete="off"
             >
                 {/*TODO: These are two types of input fields we can use, decide which one*/}
-                <TextField id="outlined-basic" label="Username" variant="outlined" />
-                <TextField id="standard-basic" label="Username" variant="standard" />
+                <TextField id="outlined-basic" label="Username" variant="outlined" onChange={(event) => setUser(event.target.value)} />
+                <Stack direction="row" spacing={2}>
+                    <Button type="submit" variant="contained">Login</Button>
+                </Stack>
             </Box>
-            <Stack direction="row" spacing={2}>
-                <Button variant="contained">Contained</Button>
-            </Stack>
         </div>
     )
 }
---
+++ PlantHub01/Models/Login.cs
@@ -0,0 +1,7 @@
+﻿namespace PlantHub01.Models
+{
+    public class Login
+    {
+        public string Username { get; set; } = string.Empty;
+    }
+}
