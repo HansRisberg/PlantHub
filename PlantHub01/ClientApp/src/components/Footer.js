6fc51f7dd363b7ae1eeaddfee6d28ffd61576138
HaaVad <123933986+HaaVad@users.noreply.github.com>
2023-04-19 20:11:46 +0200

    Tiny update

--- PlantHub01/ClientApp/src/components/Footer.js
+++ PlantHub01/ClientApp/src/components/Footer.js
@@ -3,6 +3,10 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
+import TextField from '@mui/material/TextField';
+import logo from '../assets/PlantHubLogo2.png';
+
+
 
 
 export const Footer = () => {
@@ -10,7 +14,7 @@ export const Footer = () => {
         <Paper
             sx={{
                 marginTop: "20px",
-                height: "200px",
+                height: "400px",
                 bottom: 0,
                 width: "100%",
                 zIndex: 1300,
@@ -26,18 +30,24 @@ export const Footer = () => {
                         display: "flex",
                         flexDirection: "column",
                         alignItems: "center",
-                        marginTop: "40px",
+                        marginTop: "120px",
                     }}
-                >
-                <h1>Sign up for our newsletter!</h1>
-                    <Typography variant="caption" sx={{ mt: 1.5 }}>
+                >       
+                    <Box sx={{ height: "36px" }} />
+                    <h1>Sign up for our newsletters!</h1>
+                    <Typography variant="caption" sx={{ mt: 1.5, marginTop: "2px" }}>
+                        Get monthly updates with plant tips, invitations to events and other fun stuff!
+                    </Typography>
+                    <TextField placeholder="Enter E-mail here!" sx={{ width: "50%", opacity: 0.9 }} />
+                    <Typography variant="caption" sx={{ mt: 1.5, marginTop: "20px" }}>
                         Contact us at heyheyhey@planthub.no
-
                     </Typography>
                     {/* e.g., input field, submit button */}
                     <Typography variant="caption" sx={{ mt: 1.5 }}>
                         PlantHub 2023
                     </Typography>
+                    <box><img sx={{ marginTop: "50px" }} src={logo} width="170px" /></box>
+
                 </Box>
             </Container>
         </Paper>
